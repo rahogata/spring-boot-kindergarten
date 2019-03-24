38bebdec188d34e12270aa3eb338ec662ded2568
Shiva Kumar K R <shiva.k@securelyshare.com>
2019-03-24 20:30:37 +0530

    show error page based per status codes.

---
+++ src/main/java/in/co/rahogata/springboot/SpringBootKindergartenApplication.java
@@ -0,0 +1,13 @@
+package in.co.rahogata.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootKindergartenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootKindergartenApplication.class, args);
+	}
+
+}
---
+++ src/main/java/in/co/rahogata/springboot/rest/HomeController.java
@@ -0,0 +1,13 @@
+package in.co.rahogata.springboot.rest;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/")
+    public String indexPage() {
+        return "index";
+    }
+}
