ddd756c9ee5e9706573df6e75d1eee7e05824764
Shiva Kumar <shiva20grk@gmail.com>
2019-11-01 19:55:48 +0530

    tests with shared context I believe.

--- src/test/java/in/co/rahogata/SpringContextInTests.java
+++ src/test/java/in/co/rahogata/SpringContextInTests.java
@@ -1,15 +1,8 @@
 package in.co.rahogata;
 
-import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
-
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
 
-@ExtendWith(SpringExtension.class)
-@SpringBootTest(webEnvironment = RANDOM_PORT)
-public class SpringContextInTests {
+public class SpringContextInTests extends GeneralTestSuite {
 
     @Test
     public void testA() {
