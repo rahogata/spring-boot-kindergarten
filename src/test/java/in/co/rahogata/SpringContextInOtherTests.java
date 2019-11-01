04d9c1c85d9f42e42deee18715258d04f4ed4473
Shiva Kumar <shiva20grk@gmail.com>
2019-11-01 20:17:02 +0530

    test class inheritance also reuses context.

--- src/test/java/in/co/rahogata/SpringContextInOtherTests.java
+++ src/test/java/in/co/rahogata/SpringContextInOtherTests.java
@@ -1,16 +1,9 @@
 package in.co.rahogata;
 
-import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
-
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
 
-@ExtendWith(SpringExtension.class)
-@SpringBootTest(webEnvironment = RANDOM_PORT)
-public class SpringContextInOtherTests {
+public class SpringContextInOtherTests extends GeneralTestSuite {
 
     @Autowired
     LeafService leafService;
