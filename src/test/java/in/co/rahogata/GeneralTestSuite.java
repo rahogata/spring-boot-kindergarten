29e415c4360744087fc6bde6447c5237d84a78c9
Shiva Kumar <shiva20grk@gmail.com>
2019-11-01 20:18:05 +0530

    @DirtiesContext disables reusing context across tests.

--- src/test/java/in/co/rahogata/GeneralTestSuite.java
+++ src/test/java/in/co/rahogata/GeneralTestSuite.java
@@ -4,10 +4,12 @@ import static org.springframework.boot.test.context.SpringBootTest.WebEnvironmen
 
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 @ExtendWith(SpringExtension.class)
 @SpringBootTest(webEnvironment = RANDOM_PORT)
+@DirtiesContext
 public class GeneralTestSuite {
 
 }
