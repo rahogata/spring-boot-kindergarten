aa42a16c57d5517808b1b2b0590e7220d5e24ef4
Shiva Kumar K R <shiva.k@securelyshare.com>
2020-03-04 19:54:19 +0530

    connection closed override.

--- src/main/java/in/co/rahogata/config/CertWebSocketHandler.java
+++ src/main/java/in/co/rahogata/config/CertWebSocketHandler.java
@@ -1,7 +1,10 @@
 package in.co.rahogata.config;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+import org.springframework.web.socket.CloseStatus;
 import org.springframework.web.socket.TextMessage;
 import org.springframework.web.socket.WebSocketSession;
 import org.springframework.web.socket.handler.TextWebSocketHandler;
@@ -11,6 +14,7 @@ import in.co.rahogata.service.impl.CertProviderImpl;
 @Component
 public class CertWebSocketHandler extends TextWebSocketHandler {
 
+    private static final Logger LOG = LoggerFactory.getLogger(CertWebSocketHandler.class);
     private WebSocketSession session;
     private CertProviderImpl certProviderImpl;
 
@@ -34,5 +38,13 @@ public class CertWebSocketHandler extends TextWebSocketHandler {
     public void afterConnectionEstablished(WebSocketSession session) throws Exception {
         super.afterConnectionEstablished(session);
         this.session = session;
+        LOG.info("Connection has established for {}", session);
+    }
+    
+    @Override
+    public void afterConnectionClosed(WebSocketSession session, CloseStatus status) throws Exception {
+        // TODO Auto-generated method stub
+        super.afterConnectionClosed(session, status);
+        LOG.info("Connection closed by {}", session);
     }
 }
