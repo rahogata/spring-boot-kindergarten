542583745e3d136bacf672f317d33686fc8ae9b5
rahogata <shivakumarkr1@gmail.com>
2024-04-06 15:08:09 +0530

    add main app

---
+++ src/main/java/in/co/rahogata/Dwara.java
@@ -0,0 +1,12 @@
+package in.co.rahogata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Dwara {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Dwara.class, args);
+    }
+}
