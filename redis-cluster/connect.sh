575a8e68e440b79aad4ec9c5598d99c2172f6eb4
rahogata <shivakumarkr1@gmail.com>
2024-06-30 00:09:47 +0530

    clean up.

--- redis-cluster/connect.sh
+++ redis-cluster/connect.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 
-# redis-cli -p 7000 -a bitnami --tls --cert ./tls/client.crt --key ./tls/client.key --cacert ./tls/ca.crt "$@"
- redis-cli -p 7000 -a bitnami --tls --cert ./tls/client1.crt --key ./tls/client1.key --cacert ./tls/ca.crt "$@"
+ redis-cli -p 7000 -a bitnami --tls --cert ./tls/client.crt --key ./tls/client.key --cacert ./tls/ca.crt "$@"
+# redis-cli -p 7000 -a bitnami --tls --cert ./tls/client1.crt --key ./tls/client1.key --cacert ./tls/ca.crt "$@"
